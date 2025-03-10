9d942e3787407bb579e57594aafde9fdfe1c352c
Widyasmaraafif <asus@widyasmara-program.europe-north1-b.c.mentoring-batch-1.internal>
2025-03-10 06:16:16 +0000

    update

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -12,7 +12,7 @@ class TrustProxies extends Middleware
      *
      * @var array<int, string>|string|null
      */
-    protected $proxies = ['172.16.25.2'];
+    protected $proxies;
 
     /**
      * The headers that should be used to detect proxies.
